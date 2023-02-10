7510e4e738bb784f461fced77899e29b6c4da521
100472092 <100472092@alumnos.uc3m.es>
2023-02-10 14:40:45 +0100

    validation funtion y toca comprobar si funciona

--- UC3MLogistics/OrderManager.py
+++ UC3MLogistics/OrderManager.py
@@ -2,16 +2,33 @@ import json
 from .OrderMangementException import OrderManagementException
 from .OrderRequest import OrderRequest
 
+
 class OrderManager:
     def __init__(self):
         pass
 
-    def ValidateEAN13( self, eAn13 ):
-        # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
-        # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
+    def ValidateEAN13(self, eAn13):
+        if len(eAn13) == 13:
+            return False
+
+        sumOdd = 0
+        sumEven = 0
+
+        for i in range(len(eAn13)-1):
+            if i % 2 == 0:
+                sumEven += int(eAn13[i])
+            else:
+                sumOdd += int(eAn13[i])
+
+        sumEven *= 3
+        validation = 10 - ((sumOdd + sumEven) % 10)
+
+        if (int(eAn13[-1]) != validation):
+            return False
+
         return True
 
-    def ReadproductcodefromJSON( self, fi ):
+    def ReadproductcodefromJSON(self, fi):
 
         try:
             with open(fi) as f:
@@ -20,7 +37,6 @@ class OrderManager:
             raise OrderManagementException("Wrong file or file path") from e
         except json.JSONDecodeError as e:
             raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from e
-
 
         try:
             PRODUCT = DATA["id"]
